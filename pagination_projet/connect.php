dfcd4e520d486e7f2980b7ebf8e7c4c6c290babd
musescorecontributor <46889731+musescorecontributor@users.noreply.github.com>
2022-04-08 13:52:28 +0200

    Add files via upload

---
+++ pagination_projet/connect.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            
+            //On établit la connexion
+            $conn = new PDO("mysql:host=$servername;dbname=magasins", $username, $password);
+        ?>
+    </body>
+</html>
